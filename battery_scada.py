71e6d584b7f986d1a0b3ca0769676e93704c8b11
Georgi <georgi.manev@entra.energy>
2024-08-27 12:49:12 +0300

    init

--- battery_scada.py
+++ battery_scada.py
@@ -17,8 +17,8 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime, timedelta,date
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from PIL import Image, ImageDraw, ImageFont
-from waveshare_epd import epd2in7_V2
+# from PIL import Image, ImageDraw, ImageFont
+# from waveshare_epd import epd2in7_V2
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -33,7 +33,25 @@ class BatteryScada():
         self.round_trip = round_trip
         self.actual_data = {}
         self.batt_id = batt_id
+        self.get_current_state_of_charge()
 
+    
+    def get_current_state_of_charge(self):
+        # get current SoC when power ON the pi
+        session = SessionLocal()
+        try:
+            result = session.query(BatteryActualState)\
+                        .order_by(BatteryActualState.timestamp.desc())\
+                        .first()
+            if result:
+                self.state_of_charge = result.battery_state_of_charge_actual               
+            else:
+                print(f"There are no results!")
+        except Exception as e:
+            print(f"Error fetching schedule: {e}")
+            return None
+        finally:
+            session.close()  
     
 
 
@@ -114,6 +132,7 @@ class BatteryScada():
             quarter_hour = timenow.hour   
         
         target_timestamp = timenow.replace(hour=quarter_hour, minute=quarter_min, second=0, microsecond=0)
+        print(f"Target Timestamp: {target_timestamp}")
         session = SessionLocal()         
         try:
             result = session.query(BatterySchedule).filter(
@@ -204,74 +223,74 @@ class BatteryScada():
             session.close()  
         
         
-    def display_data(self):
-        print("Here")
-        soc = self.actual_data.get("soc", None)
-        invertor = self.actual_data.get("invertor", None)   
+    # def display_data(self):
+    #     print("Here")
+    #     soc = self.actual_data.get("soc", None)
+    #     invertor = self.actual_data.get("invertor", None)   
         
-        if soc is not None and invertor is not None:
-            batt_status = "Idle"
-            if invertor > 0:
-                batt_status = "Charging"
-            elif invertor < 0:
-                batt_status = "Discharging"
-            else:
-                batt_status = "Idle"
-            script_dir = os.path.dirname(os.path.realpath(__file__))
-            picdir = os.path.join(script_dir, 'pic')
-            libdir = os.path.join(script_dir, 'lib')
-            if os.path.exists(libdir):
-                sys.path.append(libdir)
-            try:
-                epd = epd2in7_V2.EPD()
-                epd.init()
-                epd.Clear()
-                font_path = os.path.join(picdir, 'Font.ttc')
-                try:
-                    font24 = ImageFont.truetype(font_path, 24)
-                    font20 = ImageFont.truetype(font_path, 18)
-                except IOError as e:                
-                    font24 = ImageFont.load_default()  # Use default font if the specified font is not found
-                    font20 = ImageFont.load_default()
-                # Prepare the image with horizontal orientation    
-                image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-                draw = ImageDraw.Draw(image)
+    #     if soc is not None and invertor is not None:
+    #         batt_status = "Idle"
+    #         if invertor > 0:
+    #             batt_status = "Charging"
+    #         elif invertor < 0:
+    #             batt_status = "Discharging"
+    #         else:
+    #             batt_status = "Idle"
+    #         script_dir = os.path.dirname(os.path.realpath(__file__))
+    #         picdir = os.path.join(script_dir, 'pic')
+    #         libdir = os.path.join(script_dir, 'lib')
+    #         if os.path.exists(libdir):
+    #             sys.path.append(libdir)
+    #         try:
+    #             epd = epd2in7_V2.EPD()
+    #             epd.init()
+    #             epd.Clear()
+    #             font_path = os.path.join(picdir, 'Font.ttc')
+    #             try:
+    #                 font24 = ImageFont.truetype(font_path, 24)
+    #                 font20 = ImageFont.truetype(font_path, 18)
+    #             except IOError as e:                
+    #                 font24 = ImageFont.load_default()  # Use default font if the specified font is not found
+    #                 font20 = ImageFont.load_default()
+    #             # Prepare the image with horizontal orientation    
+    #             image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    #             draw = ImageDraw.Draw(image)
                 
-                current_time = time.strftime('%d-%m-%Y %H:%M')
-                cell_width = 80
-                cell_height = 40
-                # Clear the entire image
-                draw.rectangle((0, 0, epd.height, epd.width), fill=255)
+    #             current_time = time.strftime('%d-%m-%Y %H:%M')
+    #             cell_width = 80
+    #             cell_height = 40
+    #             # Clear the entire image
+    #             draw.rectangle((0, 0, epd.height, epd.width), fill=255)
                 
-                draw.rectangle((0, 0, cell_width, cell_height), outline=0)
-                # Draw the current time at the top left corner
-                draw.text((8, 10), "Battery1", font=font20, fill=0)
+    #             draw.rectangle((0, 0, cell_width, cell_height), outline=0)
+    #             # Draw the current time at the top left corner
+    #             draw.text((8, 10), "Battery1", font=font20, fill=0)
                 
-                draw.rectangle((cell_width, 0, cell_width * 2+20, cell_height), outline=0)
-                # Draw "100MW" next to the time with adjusted spacing
-                draw.text((90, 10), "100MW/h", font=font20, fill=0)
+    #             draw.rectangle((cell_width, 0, cell_width * 2+20, cell_height), outline=0)
+    #             # Draw "100MW" next to the time with adjusted spacing
+    #             draw.text((90, 10), "100MW/h", font=font20, fill=0)
                 
-                draw.rectangle((cell_width, 0, cell_width * 3 +20, cell_height), outline=0)
+    #             draw.rectangle((cell_width, 0, cell_width * 3 +20, cell_height), outline=0)
 
 
-                # Draw "25MW" next to "100MW" with adjusted spacing
-                draw.text((190, 10), "25MW", font=font20, fill=0)
+    #             # Draw "25MW" next to "100MW" with adjusted spacing
+    #             draw.text((190, 10), "25MW", font=font20, fill=0)
                 
-                draw.text((8, 45), current_time, font=font20, fill=0)
+    #             draw.text((8, 45), current_time, font=font20, fill=0)
                 
-                draw.text((8, 90), f"SoC: {soc} MW/h", font=font20, fill=0)
-                draw.text((8, 120), f"{batt_status}: {invertor} MW", font=font20, fill=0)
+    #             draw.text((8, 90), f"SoC: {soc} MW/h", font=font20, fill=0)
+    #             draw.text((8, 120), f"{batt_status}: {invertor} MW", font=font20, fill=0)
 
-                # Perform a full update            
-                epd.display(epd.getbuffer(image))
-            except IOError as e:
-                logging.info(e)
-                logging.error(traceback.format_exc())
+    #             # Perform a full update            
+    #             epd.display(epd.getbuffer(image))
+    #         except IOError as e:
+    #             logging.info(e)
+    #             logging.error(traceback.format_exc())
 
-            except KeyboardInterrupt:
-                logging.info("ctrl + c:")
-                epd2in7_V2.epdconfig.module_exit(cleanup=True)
-                exit()
+    #         except KeyboardInterrupt:
+    #             logging.info("ctrl + c:")
+    #             epd2in7_V2.epdconfig.module_exit(cleanup=True)
+    #             exit()
 
     def empty_table(self):
         session = SessionLocal()
@@ -290,6 +309,7 @@ class BatteryScada():
 if __name__ == "__main__":
 
     test = BatteryScada(batt_id="batt-0001", round_trip=0.97)
+    test.actual_battery_state()
     #test.empty_table()
     #test.prepare_xls()
 
@@ -303,7 +323,7 @@ if __name__ == "__main__":
     # # Add a job to the scheduler
     scheduler.add_job(test.update_actual_battery_state_in_db, CronTrigger(minute='*'))  # This runs the job every minute
     scheduler.add_job(test.fetch_actual_db, CronTrigger(minute='*'))  # This runs the job every minute
-    scheduler.add_job(test.display_data, CronTrigger(minute='*'))
+    #scheduler.add_job(test.display_data, CronTrigger(minute='*'))
     # scheduler.add_job(test.empty_table, CronTrigger(hour=0, minute=5))
     scheduler.add_job(test.prepare_xls, CronTrigger(hour=17, minute=10))
 
---
+++ update_db_temp.py
@@ -0,0 +1,23 @@
+from database import BatterySchedule, BatteryActualState, SessionLocal
+from datetime import datetime
+
+
+
+def save_to_db(self, df):
+    timenow = datetime.now()
+    timestamp = timenow.replace(second=0, microsecond=0)
+    session = SessionLocal()
+    try:
+        actual_state_entry = BatteryActualState(
+                timestamp = timestamp,
+                battery_state_of_charge_actual = 63.12, 
+                last_min_flow = 0.16,
+                invertor_power_actual = 9.7,              
+            )
+        session.add(actual_state_entry)
+        session.commit()  # Commit the transaction
+    except Exception as e:
+        session.rollback()  # Rollback in case of an error
+        print(f"Error saving status to DB: {e}")
+    finally:
+        session.close()  # Close the session
