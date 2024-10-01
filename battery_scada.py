37a9416a4b772ab128c30ef419fbc9a4fef7d61e
Georgi <georgi.manev@entra.energy>
2024-10-01 18:30:01 +0300

    fetch data from api

--- battery_scada.py
+++ battery_scada.py
@@ -18,8 +18,8 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime, timedelta,date
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from PIL import Image, ImageDraw, ImageFont
-from waveshare_epd import epd2in7_V2
+# from PIL import Image, ImageDraw, ImageFont
+# from waveshare_epd import epd2in7_V2
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -72,53 +72,91 @@ class BatteryScada():
         #return name_date == d1 
         file_name = file.split("_")[0]
         return file_name == "ZUSE"
+    
+    def fetch_schedule_endpoint(self):
+
+        try:
+            url = "http://85.14.6.37:16543/api/schedule/"
+            response = requests.get(url)
+            data = response.json()
+
+        
+            filtered_data = [entry for entry in data if entry['devId'] == self.batt_id]
+
+            df = pd.DataFrame(filtered_data)
+
+            # Ensure timestamp column is parsed correctly
+            df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')  # Use errors='coerce' to handle potential conversion errors
+            df.set_index('timestamp', inplace=True)
+
+            # Assuming 'df' is your DataFrame
+            df = df[['invertor']]  # Keep only the 'invertor' column
+            df['timestamp'] = df.index  # Add the index as a new 'timestamp' column
+
+            # Rename the 'invertor' column to 'schedule'
+            df = df.rename(columns={'invertor': 'schedule'})
+
+            df = df.reset_index(drop=True)
+
+            # Reorder the DataFrame to have 'timestamp' first
+            df = df[['timestamp', 'schedule']]
+
+            self.save_to_db(df)
+
+        except Exception as e:
+            logging.error(f"Error occurred while fetching the endpoint: {e}") 
+
         
     
 
-    def prepare_xls(self):
-        try:          
-            fn = "schedules"
-            for root, dirs, files in os.walk(fn):                
-                xlsfiles = [f for f in files if f.endswith('.xls')]
-                for xlsfile in xlsfiles:
-                    my_file = self.get_file_name(xlsfile)                   
-                    if my_file:
+    # def prepare_xls(self):
+    #     try:          
+    #         fn = "schedules"
+    #         for root, dirs, files in os.walk(fn):                
+    #             xlsfiles = [f for f in files if f.endswith('.xls')]
+    #             for xlsfile in xlsfiles:
+    #                 my_file = self.get_file_name(xlsfile)                   
+    #                 if my_file:
                         
-                        filepath = os.path.join(fn, xlsfile)                        
-                        excel_workbook = xlrd.open_workbook(filepath)
-                        excel_worksheet = excel_workbook.sheet_by_index(0)  
-                        #Day ahead!!!
-                        xl_date = date.today() + timedelta(days=1)
-                        xl_date_time = str(xl_date) + "T01:15:00"
-                        period = (24 * 4) 
-                        schedule_list = []
-                        i = 0
-                        timeIndex = pd.date_range(start=xl_date_time, periods=period, freq="0h15min")
-                        while i < period:
-                            i += 1
-                            xl_schedule = excel_worksheet.cell_value(10, 2 + i)  
-                            schedule_list.append(xl_schedule)
-                        df = pd.DataFrame(schedule_list, index=timeIndex)
-                        df.columns = ['schedule']
-                        self.save_to_db(df)
+    #                     filepath = os.path.join(fn, xlsfile)                        
+    #                     excel_workbook = xlrd.open_workbook(filepath)
+    #                     excel_worksheet = excel_workbook.sheet_by_index(0)  
+    #                     #Day ahead!!!
+    #                     xl_date = date.today() + timedelta(days=1)
+    #                     xl_date_time = str(xl_date) + "T01:15:00"
+    #                     period = (24 * 4) 
+    #                     schedule_list = []
+    #                     i = 0
+    #                     timeIndex = pd.date_range(start=xl_date_time, periods=period, freq="0h15min")
+    #                     while i < period:
+    #                         i += 1
+    #                         xl_schedule = excel_worksheet.cell_value(10, 2 + i)  
+    #                         schedule_list.append(xl_schedule)
+    #                     df = pd.DataFrame(schedule_list, index=timeIndex)
+    #                     df.columns = ['schedule']
+    #                     print(df.head())
+    #                     self.save_to_db(df)
 
-        except Exception as e:
-            logging.error(f"Error occurred while preparing the Excel file: {e}") 
+    #     except Exception as e:
+    #         logging.error(f"Error occurred while preparing the Excel file: {e}") 
 
     def save_to_db(self, df):
         # Get the database session
         session = SessionLocal()
         try:
-            for row in df.itertuples():
-                
-                schedule_entry = BatterySchedule(
-                    timestamp=row.Index,
-                    battery_state="battery_state",
-                    schedule=row.schedule,               
-                )
-
-                # Add the entry to the session
-                session.add(schedule_entry)
+            for row in df.itertuples():     
+                # Check if the entry with the same timestamp exists
+                existing_entry = session.query(BatterySchedule).filter_by(timestamp=row.timestamp).first()           
+                if existing_entry:                               
+                    existing_entry.schedule = row.schedule
+                else:
+                    # Create a new entry if it does not exist
+                    schedule_entry = BatterySchedule(
+                        timestamp=row.timestamp,
+                        battery_state="battery_state",  # You may want to adjust this value as needed
+                        schedule=row.schedule,
+                    )
+                    session.add(schedule_entry)  # Add the new entry to the session
                 session.commit()  # Commit the transaction
         except Exception as e:
             session.rollback()  # Rollback in case of an error
@@ -138,7 +176,8 @@ class BatteryScada():
         target_timestamp = timenow.replace(hour=quarter_hour, minute=quarter_min, second=0, microsecond=0)
         print(f"Target Timestamp: {target_timestamp}")
         session = SessionLocal()         
-        try:
+        try:   
+            
             result = session.query(BatterySchedule).filter(
                 BatterySchedule.timestamp == target_timestamp
             ).first()
@@ -206,7 +245,7 @@ class BatteryScada():
         timestamp_min_res = timenow.replace(second=0, microsecond=0)
         timestamp_previous_min = timestamp_min_res - timedelta(minutes=1)
         print(f"Requested Timestamp Previous Min: {timestamp_previous_min}")
-        session = SessionLocal()       
+        session = SessionLocal()              
         try:
             result = session.query(BatteryActualState).filter(
                     BatteryActualState.timestamp == timestamp_previous_min
@@ -253,71 +292,71 @@ class BatteryScada():
             except requests.exceptions.RequestException as e:
                 print(f"Failed to publish {pin}: {value}, Error: {e}")
         
-    def display_data(self, soc, invertor):       
+    # def display_data(self, soc, invertor):       
         
-        if soc is not None and invertor is not None:
-            batt_status = "Idle"
-            if invertor > 0:
-                batt_status = "Charging"
-            elif invertor < 0:
-                batt_status = "Discharging"
-            else:
-                batt_status = "Idle"
-            script_dir = os.path.dirname(os.path.realpath(__file__))
-            picdir = os.path.join(script_dir, 'pic')
-            libdir = os.path.join(script_dir, 'lib')
-            if os.path.exists(libdir):
-                sys.path.append(libdir)
-            try:
-                epd = epd2in7_V2.EPD()
-                epd.init()
-                epd.Clear()
-                font_path = os.path.join(picdir, 'Font.ttc')
-                try:
-                    font24 = ImageFont.truetype(font_path, 24)
-                    font20 = ImageFont.truetype(font_path, 18)
-                except IOError as e:                
-                    font24 = ImageFont.load_default()  # Use default font if the specified font is not found
-                    font20 = ImageFont.load_default()
-                # Prepare the image with horizontal orientation    
-                image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-                draw = ImageDraw.Draw(image)
+    #     if soc is not None and invertor is not None:
+    #         batt_status = "Idle"
+    #         if invertor > 0:
+    #             batt_status = "Charging"
+    #         elif invertor < 0:
+    #             batt_status = "Discharging"
+    #         else:
+    #             batt_status = "Idle"
+    #         script_dir = os.path.dirname(os.path.realpath(__file__))
+    #         picdir = os.path.join(script_dir, 'pic')
+    #         libdir = os.path.join(script_dir, 'lib')
+    #         if os.path.exists(libdir):
+    #             sys.path.append(libdir)
+    #         try:
+    #             epd = epd2in7_V2.EPD()
+    #             epd.init()
+    #             epd.Clear()
+    #             font_path = os.path.join(picdir, 'Font.ttc')
+    #             try:
+    #                 font24 = ImageFont.truetype(font_path, 24)
+    #                 font20 = ImageFont.truetype(font_path, 18)
+    #             except IOError as e:                
+    #                 font24 = ImageFont.load_default()  # Use default font if the specified font is not found
+    #                 font20 = ImageFont.load_default()
+    #             # Prepare the image with horizontal orientation    
+    #             image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    #             draw = ImageDraw.Draw(image)
                 
-                current_time = time.strftime('%d-%m-%Y %H:%M')
-                cell_width = 80
-                cell_height = 40
-                # Clear the entire image
-                draw.rectangle((0, 0, epd.height, epd.width), fill=255)
+    #             current_time = time.strftime('%d-%m-%Y %H:%M')
+    #             cell_width = 80
+    #             cell_height = 40
+    #             # Clear the entire image
+    #             draw.rectangle((0, 0, epd.height, epd.width), fill=255)
                 
-                draw.rectangle((0, 0, cell_width, cell_height), outline=0)
-                # Draw the current time at the top left corner
-                draw.text((8, 10), "Battery1", font=font20, fill=0)
+    #             draw.rectangle((0, 0, cell_width, cell_height), outline=0)
+    #             # Draw the current time at the top left corner
+    #             draw.text((8, 10), "Battery1", font=font20, fill=0)
                 
-                draw.rectangle((cell_width, 0, cell_width * 2+20, cell_height), outline=0)
-                # Draw "100MW" next to the time with adjusted spacing
-                draw.text((90, 10), "100MW/h", font=font20, fill=0)
+    #             draw.rectangle((cell_width, 0, cell_width * 2+20, cell_height), outline=0)
+    #             # Draw "100MW" next to the time with adjusted spacing
+    #             draw.text((90, 10), "100MW/h", font=font20, fill=0)
                 
-                draw.rectangle((cell_width, 0, cell_width * 3 +20, cell_height), outline=0)
+    #             draw.rectangle((cell_width, 0, cell_width * 3 +20, cell_height), outline=0)
 
 
-                # Draw "25MW" next to "100MW" with adjusted spacing
-                draw.text((190, 10), "25MW", font=font20, fill=0)
+    #             # Draw "25MW" next to "100MW" with adjusted spacing
+    #             draw.text((190, 10), "25MW", font=font20, fill=0)
                 
-                draw.text((8, 45), current_time, font=font20, fill=0)
+    #             draw.text((8, 45), current_time, font=font20, fill=0)
                 
-                draw.text((8, 90), f"SoC: {soc} MW/h", font=font20, fill=0)
-                draw.text((8, 120), f"{batt_status}: {invertor} MW", font=font20, fill=0)
+    #             draw.text((8, 90), f"SoC: {soc} MW/h", font=font20, fill=0)
+    #             draw.text((8, 120), f"{batt_status}: {invertor} MW", font=font20, fill=0)
 
-                # Perform a full update            
-                epd.display(epd.getbuffer(image))
-            except IOError as e:
-                logging.info(e)
-                logging.error(traceback.format_exc())
+    #             # Perform a full update            
+    #             epd.display(epd.getbuffer(image))
+    #         except IOError as e:
+    #             logging.info(e)
+    #             logging.error(traceback.format_exc())
 
-            except KeyboardInterrupt:
-                logging.info("ctrl + c:")
-                epd2in7_V2.epdconfig.module_exit(cleanup=True)
-                exit()
+    #         except KeyboardInterrupt:
+    #             logging.info("ctrl + c:")
+    #             epd2in7_V2.epdconfig.module_exit(cleanup=True)
+    #             exit()
 
     def empty_table(self):
         session = SessionLocal()
@@ -336,9 +375,9 @@ class BatteryScada():
 if __name__ == "__main__":
 
     test = BatteryScada(batt_id="batt-0001", round_trip=0.97)
-    test.get_current_state_of_charge()
-    #test.empty_table()
-    #test.prepare_xls()
+    #test.get_current_state_of_charge()
+    test.empty_table()
+    #test.fetch_schedule_endpoint()
 
 
     # Connect to the MQTT broker
@@ -346,13 +385,15 @@ if __name__ == "__main__":
     mqtt_client.connect_client()
     # # Create a scheduler instance
     scheduler = BackgroundScheduler()
+
+    scheduler.add_job(test.fetch_schedule_endpoint, CronTrigger(minute='*'))
     
     # # Add a job to the scheduler
     scheduler.add_job(test.update_actual_battery_state_in_db, CronTrigger(minute='*'))  # This runs the job every minute
     scheduler.add_job(test.fetch_actual_db, CronTrigger(minute='*'))  # This runs the job every minute
     
     # scheduler.add_job(test.empty_table, CronTrigger(hour=0, minute=5))
-    scheduler.add_job(test.prepare_xls, CronTrigger(hour=17, minute=10))
+    
 
     scheduler.start()
 
